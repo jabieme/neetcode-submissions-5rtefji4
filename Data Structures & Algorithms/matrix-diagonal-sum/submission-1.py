8cea9c8459a339d9f3b8115a21c7c0aa0f163977
jabieme <jemeka2@oberlin.edu>
2026-03-31 13:17:10 -0400

    Add: matrix-diagonal-sum - submission-1

---
+++ Data Structures & Algorithms/matrix-diagonal-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        iterator = [0,0]
+        iterator2 = [0, len(mat[0])-1]
+        res = 0
+        visited = set()
+        
+        while iterator != [len(mat), len(mat)]:
+            iteration = [iterator[0],iterator[1]]
+            iteration2 = [iterator2[0],iterator2[1]]
+            if tuple(iteration) not in visited:
+                res+=mat[iteration[0]][iteration[1]]
+            if iteration != iteration2:
+                res+=mat[iteration2[0]][iteration2[1]]
+            visited.add(tuple(iteration))
+            iterator[0]+=1
+            iterator[1]+=1
+            iterator2[0]+=1
+            iterator2[1]-=1
+        return res
